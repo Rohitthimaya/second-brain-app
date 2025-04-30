de0dcb46d8fc55dca1a4f2673a41054c44a142b8
Rohit Thimaya <rohitthimaya@Rohits-MacBook-Pro.local>
2025-04-29 19:05:53 -0700

    Hashed password

---
+++ src/db/userSchema.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchemaDB = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+})
+
+const User = mongoose.model("User", userSchemaDB);
+export default User;
--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,53 @@ import bodyParser from "body-parser";
 import express from "express";
 import { userSchema, userSchemaTs } from "./validations/validations";
 import {connectDB} from "./db/db"
+import User from "./db/userSchema";
+import bcrypt from "bcrypt";
+
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB()
 
-// Sign up
-app.post("/api/v1/signup", (req, res) => {
-    // zod validations
-    const parsedObj = userSchema.safeParse(req.body);
-
-    if (!parsedObj.success) {
-        res.status(411).json({
-            error: parsedObj.error.flatten().fieldErrors
+// Sign Up
+app.post("/api/v1/signup", async (req, res) => {
+    try {
+      const parsedObj = userSchema.safeParse(req.body);
+  
+      if (!parsedObj.success) {
+        res.status(400).json({
+          error: parsedObj.error.flatten().fieldErrors,
         });
+        return;
+      }
+  
+      const { username, password } = parsedObj.data;
+  
+      const userExist = await User.findOne({ username });
+  
+      if (userExist) {
+        res.status(409).json({ message: "User already exists!" });
+        return
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
+  
+      res.status(201).json({ message: "User created successfully." });
+      return 
+  
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Something went wrong." });
+      return 
     }
-
-    const updatedBody: userSchemaTs = req.body
-
-    // Check if user exist = middleware
-    // Create user and add into mongo db
-    // provide jwt token
-    // redirect to main page
-})
+  });
+  
 
 app.listen(3000, () => {
     console.log(`App running on port: ${3000}`)
